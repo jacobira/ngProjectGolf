a41dc715528d9ea5cfc42ab3a6d72a610e1830d4
Jacob Baldwin <jacobbaldwin@Jacobs-MacBook-Pro.local>
2018-08-02 13:16:48 -0600

    Initialization of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SetupComponent } from './setup/setup.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SetupComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
